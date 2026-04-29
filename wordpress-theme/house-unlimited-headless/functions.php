291bc2bccb6b2e59391823cb78d18ed781caf8a4
IsahPhilip <Isahphilip50@gmail.com>
2026-04-29 15:48:27 +0100

    changes

--- wordpress-theme/house-unlimited-headless/functions.php
+++ wordpress-theme/house-unlimited-headless/functions.php
@@ -23,6 +23,128 @@ add_action(
 				'footer'  => __( 'Footer Menu', 'house-unlimited-headless' ),
 			)
 		);
+
+		// Register Property Custom Post Type for GraphQL
+		register_post_type(
+			'property',
+			array(
+				'labels'             => array(
+					'name'               => __( 'Properties', 'house-unlimited-headless' ),
+					'singular_name'      => __( 'Property', 'house-unlimited-headless' ),
+					'add_new'            => __( 'Add New Property', 'house-unlimited-headless' ),
+					'add_new_item'       => __( 'Add New Property', 'house-unlimited-headless' ),
+					'edit_item'          => __( 'Edit Property', 'house-unlimited-headless' ),
+					'new_item'           => __( 'New Property', 'house-unlimited-headless' ),
+					'view_item'          => __( 'View Property', 'house-unlimited-headless' ),
+					'search_items'       => __( 'Search Properties', 'house-unlimited-headless' ),
+					'not_found'          => __( 'No properties found', 'house-unlimited-headless' ),
+					'not_found_in_trash' => __( 'No properties found in trash', 'house-unlimited-headless' ),
+				),
+				'public'             => true,
+				'has_archive'        => true,
+				'show_in_rest'       => true,
+				'show_in_graphql'    => true,
+				'graphql_single_name' => 'Property',
+				'graphql_plural_name' => 'Properties',
+				'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+				'menu_icon'          => 'dashicons-building',
+				'rewrite'            => array( 'slug' => 'properties' ),
+			)
+		);
+
+		// Register Property Fields for ACF if using ACF
+		if ( function_exists( 'acf_add_local_field_group' ) ) {
+			acf_add_local_field_group( array(
+				'key'                   => 'group_property_fields',
+				'title'                 => 'Property Fields',
+				'fields'                => array(
+					array(
+						'key'               => 'field_price',
+						'label'             => 'Price',
+						'name'              => 'price',
+						'type'              => 'text',
+					),
+					array(
+						'key'               => 'field_property_type',
+						'label'             => 'Property Type',
+						'name'              => 'property_type',
+						'type'              => 'text',
+					),
+					array(
+						'key'               => 'field_location',
+						'label'             => 'Location',
+						'name'              => 'location',
+						'type'              => 'text',
+					),
+					array(
+						'key'               => 'field_bedrooms',
+						'label'             => 'Bedrooms',
+						'name'              => 'bedrooms',
+						'type'              => 'number',
+					),
+					array(
+						'key'               => 'field_bathrooms',
+						'label'             => 'Bathrooms',
+						'name'              => 'bathrooms',
+						'type'              => 'number',
+					),
+					array(
+						'key'               => 'field_area',
+						'label'             => 'Area',
+						'name'              => 'area',
+						'type'              => 'text',
+					),
+					array(
+						'key'               => 'field_property_status',
+						'label'             => 'Property Status',
+						'name'              => 'property_status',
+						'type'              => 'text',
+					),
+					array(
+						'key'               => 'field_gallery',
+						'label'             => 'Gallery',
+						'name'              => 'gallery',
+						'type'              => 'gallery',
+					),
+				),
+				'location'              => array(
+					array(
+						array(
+							'param'    => 'post_type',
+							'operator' => '==',
+							'value'    => 'property',
+						),
+					),
+				),
+				'show_in_graphql'       => true,
+				'graphql_field_name'    => 'propertyFields',
+			) );
+		}
+	}
+);
+
+add_action(
+	'graphql_register_types',
+	static function () {
+		// Register menu locations for GraphQL
+		if ( function_exists( 'register_graphql_enum_type' ) ) {
+			register_graphql_enum_type(
+				'MenuLocationEnum',
+				array(
+					'description' => __( 'Menu locations', 'house-unlimited-headless' ),
+					'values'      => array(
+						'PRIMARY' => array(
+							'value'       => 'primary',
+							'description' => __( 'Primary menu location', 'house-unlimited-headless' ),
+						),
+						'FOOTER'  => array(
+							'value'       => 'footer',
+							'description' => __( 'Footer menu location', 'house-unlimited-headless' ),
+						),
+					),
+				)
+			);
+		}
 	}
 );
 
