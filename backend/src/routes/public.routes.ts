6a904677e51812e184a21443ca20ba6da22327d1
IsahPhilip <Isahphilip50@gmail.com>
2026-05-27 04:34:26 +0100

    changes

--- backend/src/routes/public.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getPublicMedia, getPublicSiteContent } from '../controllers/public.controller.js';
-
-const router = express.Router();
-
-router.get('/media', getPublicMedia);
-router.get('/site-content', getPublicSiteContent);
-
-export default router;
